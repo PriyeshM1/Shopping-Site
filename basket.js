cfdf3a51181c9ec3509cbac8cd30baaf3dd6aa87
Priyesh Mistry <Priyesh Mistry>
2017-03-06 16:46:47 +0000

    Grand Total on Basket

--- basket.js
+++ basket.js
@@ -1,15 +1,15 @@
-	function Product(id, name, price) {
-	    this.id = id;
-	    this.name = name;
-	    this.price = price;
-	}
+function Product(id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+}
 
 
-	var products = [
-		new Product("0", "Playstation 4", 199.99),
-		new Product("1", "XBox One", 249.99),
-		new Product("2", "Wii U", 149.99),
-		]; 
+var products = [
+    new Product("0", "Playstation 4", 199.99),
+    new Product("1", "XBox One", 249.99),
+    new Product("2", "Wii U", 149.99),
+    ]; 
 
 function Item (product, price, quantity) {
     this.product = product
@@ -67,10 +67,6 @@ basket: [],
         this.basket = [];
     },
 
-    total : function() {  
-        return this.basket.reduce(function(acc, item){ acc + item.price * item.quantity }, 0)
-    },
-    
     toString : function(){
         return JSON.stringify(this.basket);
     },
@@ -79,45 +75,45 @@ basket: [],
 }
 
 function handle(req, res, command){
-switch(command[1]){
-        case "list":
-            var data = this.toString();
-            res.setHeader("Content-Type" , "application/json");
-            res.end(data);
-            break;
-        case "addItem":
-            var productId = command[2];
-            var quantity = 1;
-            this.addItem(productId, quantity)
-            res.writeHead(302, {
-               'Location' : "/basket.html" 
-            });
-            res.end();
-            break;
-        case "removeItem":
-            var productId = command[2];
-            var quantity = 1;
-            this.removeItem(productId, quantity)
-            res.writeHead(302, {
-               'Location' : "/basket.html" 
-            });
-            res.end();
-            break;
-        case "removeAll":
-            var productId = command[2];
-            this.removeAll(productId)
-            res.writeHead(302, {
-               'Location' : "/basket.html" 
-            });
-            res.end();
-            break;
-        case "clearBasket":
-            var productId = command[2];
-            this.clearBasket(productId)
-            res.writeHead(302, {
-               'Location' : "/basket.html" 
-            });
-            res.end();
-            break;
+    switch(command[1]){
+            case "list":
+                var data = this.toString();
+                res.setHeader("Content-Type" , "application/json");
+                res.end(data);
+                break;
+            case "addItem":
+                var productId = command[2];
+                var quantity = 1;
+                this.addItem(productId, quantity)
+                res.writeHead(302, {
+                   'Location' : "/basket.html" 
+                });
+                res.end();
+                break;
+            case "removeItem":
+                var productId = command[2];
+                var quantity = 1;
+                this.removeItem(productId, quantity)
+                res.writeHead(302, {
+                   'Location' : "/basket.html" 
+                });
+                res.end();
+                break;
+            case "removeAll":
+                var productId = command[2];
+                this.removeAll(productId)
+                res.writeHead(302, {
+                   'Location' : "/basket.html" 
+                });
+                res.end();
+                break;
+            case "clearBasket":
+                var productId = command[2];
+                this.clearBasket(productId)
+                res.writeHead(302, {
+                   'Location' : "/basket.html" 
+                });
+                res.end();
+                break;
     }
 }
