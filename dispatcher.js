f4a3bf5ce67936c0f9b0c1b1ef91288652d9b746
Priyesh Mistry <Priyesh Mistry>
2017-03-07 14:05:58 +0000

    Basket Updater Seperated

--- dispatcher.js
+++ dispatcher.js
@@ -2,16 +2,22 @@ var fs = require("fs");
 var basket = require("./basket");
 
 function dispatch(req, res){
+    var pattern = /.\.(html|js|css)/
     var command = req.url.substring(1).split("/")
-    switch(command[0]){
-        case "index.html": renderIndex(req, res, command); break;
-        case "basket.html": renderBasket(req, res, command); break;
-        case "basket": basket.handle(req, res, command); break;
-        default: 
-            res.writeHead(404)
-            res.end()
+    if (pattern.test(command[0])){ 
+        render("./" + command[0], req, res)
+    } else {
+        switch(command[0]){
+            case "basket": basket.handle(req, res, command); break;
+            case "basketUpdater": basket.handle(req, res, command); break;
+            default: 
+                res.writeHead(404)
+                res.end()    
+
+        }
     }
 }
+
 
 function render(path, req, res){
     fs.readFile(path, function (error, data){ 
