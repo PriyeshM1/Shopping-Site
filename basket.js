9e34efbd366bd63ec8e313ae9c8ba2a1a2391adf
Priyesh Mistry <Priyesh Mistry>
2017-03-08 13:32:34 +0000

    New products and print invoice

--- basket.js
+++ basket.js
@@ -9,6 +9,15 @@ var products = [
     new Product("0", "PlayStation 4 Pro", 349.99),
     new Product("1", "Xbox One", 224.99),
     new Product("2", "Nintendo Switch", 274.99),
+    new Product("3", "PlayStation 4 Controller", 44.99),
+    new Product("4", "Xbox One Controller", 39.99),
+    new Product("5", "HDMI Cable", 9.99),
+    new Product("6", "USB-A to USB-C Cable", 4.99),
+    new Product("7", "FIFA 17 - PS4", 29.99),
+    new Product("8", "FIFA 17 - Xbox One", 29.99),
+    new Product("9", "Call of Duty - PS4", 39.99),
+    new Product("10", "Call of Duty - Xbox One", 39.99),
+    new Product("11", "Zelda - Nintendo Switch", 29.99),
     ]; 
 
 function Item (product, price, quantity) {
@@ -66,6 +75,10 @@ basket: [],
     clearBasket : function(productId) {
         this.basket = [];
     },
+    
+    clearBasketHome : function(productId) {
+    this.basket = [];
+    },
 
     toString : function(){
         return JSON.stringify(this.basket);
@@ -112,6 +125,14 @@ function handle(req, res, command){
                 this.clearBasket(productId)
                 res.writeHead(302, {
                    'Location' : "/basket.html" 
+                });
+                res.end();
+                break;
+            case "clearBasketHome":
+                var productId = command[2];
+                this.clearBasket(productId)
+                res.writeHead(302, {
+                   'Location' : "/index.html" 
                 });
                 res.end();
                 break;
